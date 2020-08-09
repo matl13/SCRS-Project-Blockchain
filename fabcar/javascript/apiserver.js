391d67606c7bd91a6ad1fc88703edd57f3cff05e
mat_l13 <matteo.lepore13@gmail.com>
2020-08-09 11:33:35 +0200

    Aggiunta api addcar

--- fabcar/javascript/apiserver.js
+++ fabcar/javascript/apiserver.js
@@ -92,26 +92,26 @@ app.get('/api/query/:car_index', async function (req, res) {
     }
 });
 
-/*
+
 app.post('/api/addcar/', async function (req, res) {
     try {
 
         // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
-        const wallet = new FileSystemWallet(walletPath);
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        const userExists = await wallet.exists('user1');
-        if (!userExists) {
-            console.log('An identity for the user "user1" does not exist in the wallet');
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
             console.log('Run the registerUser.js application before retrying');
             return;
         }
 
         // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: false } });
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
 
         // Get the network (channel) our contract is deployed to.
         const network = await gateway.getNetwork('mychannel');
@@ -122,19 +122,18 @@ app.post('/api/addcar/', async function (req, res) {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-        await contract.submitTransaction('createCar', req.body.carid, req.body.make, req.body.model, req.body.colour, req.body.owner);
-        console.log('Transaction has been submitted');
-        res.send('Transaction has been submitted');
-
-        // Disconnect from the gateway.
-        await gateway.disconnect();
+        //creaAuto(ctx, targa, telaio, marca, modello, classeAmbientale, dataImmatricolazione, kmPercorsi, proprietario)
+        await contract.submitTransaction('creaAuto', req.body.targa, req.body.telaio, req.body.marca, req.body.modello, req.body.classeAmbientale, req.body.dataImmatricolazione, req.body.kmPercorsi, req.body.proprietario);
+        console.log(`Transaction has been evaluated`);
+        res.status(200).json({response: "Auto Creata"});
 
     } catch (error) {
-        console.error(`Failed to submit transaction: ${error}`);
+        console.error(`Failed to evaluate transaction: ${error}`);
         process.exit(1);
     }
 })
 
+/*
 app.put('/api/changeowner/:car_index', async function (req, res) {
     try {
 
