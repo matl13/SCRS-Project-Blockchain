dc4f5a27ae244f1de24ecf1b3b3891a5a916a063
Manuel <34507560+Galman21@users.noreply.github.com>
2020-08-10 19:50:32 +0200

    Aggiunto il permesso per fare richieste CORS

--- fabcar/javascript/apiserver.js
+++ fabcar/javascript/apiserver.js
@@ -4,6 +4,12 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Setting for Hyperledger Fabric
 //const { FileSystemWallet, Gateway } = require('fabric-network');
 const { Gateway, Wallets } = require('fabric-network');
