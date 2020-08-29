e75a662522bf4947550670d0f65720eb79cfb086
mat_l13 <matteo.lepore13@gmail.com>
2020-08-29 21:15:45 +0200

    file sh per enroll admin/users

---
+++ fabcar/registraUtenti.sh
@@ -0,0 +1,14 @@
+echo "###ENROLL ADMIN ORG1####"
+node javascript/enrollAdminOrg1.js 
+echo "###REGISTERING USER WEB####"
+node javascript/registerUserWeb.js
+echo "###REGISTERING USER ORG1####"
+node javascript/registerUserOrg1.js
+echo "###ENROLL ADMIN ORG2####"
+node javascript/enrollAdminOrg2.js
+echo "###REGISTERING USER ORG2####"
+node javascript/registerUserOrg2.js
+echo "###ENROLL ADMIN ORG3####"
+node javascript/enrollAdminOrg3.js
+echo "###REGISTERING USER ORG3####"
+node javascript/registerUserOrg3.js
